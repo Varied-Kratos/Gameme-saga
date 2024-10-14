4d5c5a93ffbde5a880d84b89596883bf2254f2c5
Varied-Kratos <kelehsaevalan6@gmail.com>
2024-10-14 20:19:23 +0300

    Many new things

--- levels_constuctor.py
+++ levels_constuctor.py
@@ -1,80 +1,86 @@
 import pygame
 import sys
 import os
-
+ 
 pygame.init()
-
+ 
 WIDTH, HEIGHT = 1080, 720
 ROW_COUNT = 19
 COLUMN_COUNT = 32
-
 CELL_WIDTH = WIDTH // COLUMN_COUNT
 CELL_HEIGHT = HEIGHT // ROW_COUNT
-
+ 
 screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
 pygame.display.set_caption("Level Builder")
-
+ 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
-
+ 
 bg = pygame.image.load("Images/BG.png")
 bg = pygame.transform.scale(bg, (WIDTH, HEIGHT))
-
-
+ 
+ 
 class Sprite:
-    def __init__(self, x, y, image):
-        self.image = image
+    def __init__(self, x, y, image, designation):
+        self.image = pygame.transform.scale(image, (CELL_WIDTH, CELL_HEIGHT))
         self.rect = self.image.get_rect(topleft=(x * CELL_WIDTH, y * CELL_HEIGHT))
-
+        self.designation = designation
+ 
     def draw(self, surface):
         surface.blit(self.image, self.rect.topleft)
-
-
+ 
+ 
 class Level:
     def __init__(self):
         self.sprites = []
-
-    def add_sprite(self, x, y, image):
-        new_sprite = Sprite(x, y, image)
+ 
+    def add_sprite(self, x, y, image, designation):
+        new_sprite = Sprite(x, y, image, designation)
         self.sprites.append(new_sprite)
-
+ 
     def remove_sprite(self, pos):
         for sprite in self.sprites:
             if sprite.rect.collidepoint(pos):
                 self.sprites.remove(sprite)
                 break
-
+ 
     def draw(self, surface):
         for sprite in self.sprites:
             sprite.draw(surface)
-
+ 
     def save_to_file(self, filename):
         grid = [['.' for _ in range(COLUMN_COUNT)] for _ in range(ROW_COUNT)]
         for sprite in self.sprites:
-            grid[sprite.rect.y // CELL_HEIGHT][sprite.rect.x // CELL_WIDTH] = 'X'
+            grid[sprite.rect.y // CELL_HEIGHT][sprite.rect.x // CELL_WIDTH] = sprite.designation
         with open(filename, 'w') as f:
             for row in grid:
                 f.write(' '.join(row) + '\n')
-
-
+ 
+    def clear(self):
+        self.sprites.clear()
+ 
+ 
 def load_images_from_folder(folder):
     images = {}
     for filename in os.listdir(folder):
         if filename.endswith(".png"):
             img = pygame.image.load(os.path.join(folder, filename)).convert_alpha()
-            images[filename] = img
+            images[filename[:2]] = img 
     return images
-
-
+ 
+ 
 clock = pygame.time.Clock()
 level = Level()
 images = load_images_from_folder("Tile")
 current_image = None
+current_designation = "00"
 RUN = True
 menu_visible = False
-
+instructions_visible = True
 scroll_y = 0
-
+dragging = False
+ 
+ 
 def get_filename():
     root = pygame.display.set_mode((400, 200))
     pygame.display.set_caption("Enter filename")
@@ -84,9 +90,8 @@ def get_filename():
     color_active = pygame.Color('dodgerblue2')
     color = color_inactive
     active = False
-    text = ''
+    text = 'Введите название уровня'
     done = False
-
     while not done:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -105,8 +110,10 @@ def get_filename():
                     elif event.key == pygame.K_BACKSPACE:
                         text = text[:-1]
                     else:
-                        text += event.unicode
-
+                        if text == 'Введите название уровня':
+                            text = event.unicode
+                        else:
+                            text += event.unicode
         root.fill((30, 30, 30))
         txt_surface = font.render(text, True, color)
         width = max(300, txt_surface.get_width() + 10)
@@ -115,79 +122,105 @@ def get_filename():
         pygame.draw.rect(root, color, input_box, 2)
         pygame.display.flip()
         clock.tick(30)
-
     pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
     pygame.display.set_caption("Level Builder")
     return text
-
+ 
+ 
 while RUN:
+    keys = pygame.key.get_pressed()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_f:
                 if screen.get_flags() & pygame.FULLSCREEN:
                     screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
                 else:
                     screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-
             if event.key == pygame.K_m:
                 menu_visible = not menu_visible
-
+            if event.key == pygame.K_i:
+                instructions_visible = not instructions_visible
             if event.key == pygame.K_s:
                 filename = get_filename()
                 level.save_to_file(filename + ".txt")
-
             if event.key == pygame.K_ESCAPE:
                 RUN = False
-
+            if keys[pygame.K_LCTRL] and event.key == pygame.K_d:
+                level.clear()
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1 and current_image is not None:
                 pos = event.pos
-                grid_x = int(pos[0] // CELL_WIDTH)
-                grid_y = int(pos[1] // CELL_HEIGHT)
-                level.add_sprite(grid_x, grid_y, current_image)
+                if not menu_visible or pos[0] > 160:  
+                    grid_x = int(pos[0] // CELL_WIDTH)
+                    grid_y = int(pos[1] // CELL_HEIGHT)
+                    level.add_sprite(grid_x, grid_y, current_image, current_designation)
+                    if keys[pygame.K_LSHIFT]:
+                        dragging = True
             elif event.button == 3:
                 level.remove_sprite(event.pos)
-
             if menu_visible:
-                for i, (name, img) in enumerate(images.items()):
+                for i, (designation, img) in enumerate(images.items()):
                     item_rect = pygame.Rect(10, 60 + i * (img.get_height() + 10) + scroll_y, img.get_width(), img.get_height())
                     if item_rect.collidepoint(event.pos):
                         current_image = img
-
+                        current_designation = designation
+        if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
+            dragging = False
         if event.type == pygame.MOUSEBUTTONDOWN and menu_visible and event.button == 4:
             scroll_y += 10
         elif event.type == pygame.MOUSEBUTTONDOWN and menu_visible and event.button == 5:
             scroll_y -= 10
-
+ 
+    if dragging and current_image is not None:
+        pos = pygame.mouse.get_pos()
+        grid_x = int(pos[0] // CELL_WIDTH)
+        grid_y = int(pos[1] // CELL_HEIGHT)
+        level.add_sprite(grid_x, grid_y, current_image, current_designation)
+ 
     bg_resized = pygame.transform.scale(bg, (WIDTH, HEIGHT))
     screen.blit(bg_resized, (0, 0))
-
     level.draw(screen)
-
+ 
     for x in range(0, WIDTH, CELL_WIDTH):
         pygame.draw.line(screen, (200, 200, 200), (x, 0), (x, HEIGHT))
     for y in range(0, HEIGHT, CELL_HEIGHT):
         pygame.draw.line(screen, (200, 200, 200), (0, y), (WIDTH, y))
-
+ 
     menu_button_rect = pygame.Rect(WIDTH - 150, 10, 140, 40)
     pygame.draw.rect(screen, BLACK if not menu_visible else WHITE, menu_button_rect)
     font = pygame.font.Font(None, 36)
     button_text = font.render("Menu (press M)", True, WHITE if not menu_visible else BLACK)
     screen.blit(button_text, (WIDTH - 140, 15))
-
+ 
     if menu_visible:
-        for i, (name, img) in enumerate(images.items()):
+        for i, (designation, img) in enumerate(images.items()):
             item_rect = pygame.Rect(10, 60 + i * (img.get_height() + 10) + scroll_y, img.get_width(), img.get_height())
             screen.blit(img, item_rect.topleft)
-            text_surface = font.render(name, True, BLACK)
+            text_surface = font.render(designation, True, BLACK)
             text_rect = text_surface.get_rect(topleft=(item_rect.right + 10, item_rect.top))
             screen.blit(text_surface, text_rect)
-
+ 
+    if instructions_visible:
+        instructions_bg = pygame.Surface((620, 250))
+        instructions_bg.fill(BLACK)
+        screen.blit(instructions_bg, (10, HEIGHT - 300))
+        instructions = [
+            "Инструкция:",
+            "M - Меню",
+            "I - Скрыть/открыть инструкцию",
+            "S - Save ",
+            "Shift(удерж.) + ЛКМ - Заполнить соседние клетки",
+            "Ctrl + D - Удалить все спрайты",
+            "Right Click - Удалить спрайт"
+        ]
+        for i, instruction in enumerate(instructions):
+            instruction_surface = font.render(instruction, True, WHITE)
+            screen.blit(instruction_surface, (20, HEIGHT - 290 + 30 * i))
+ 
     pygame.display.flip()
     clock.tick(60)
-
+ 
 pygame.quit()
