6b10b649dce7d5bdd3133df3dda714a6c25c0845
Varied-Kratos <kelehsaevalan6@gmail.com>
2024-10-14 15:16:37 +0300

    Save to txt file with able to enter name of level

--- levels_constuctor.py
+++ levels_constuctor.py
@@ -4,11 +4,9 @@ import os
 
 pygame.init()
 
-
 WIDTH, HEIGHT = 1080, 720
 ROW_COUNT = 19
 COLUMN_COUNT = 32
-
 
 CELL_WIDTH = WIDTH // COLUMN_COUNT
 CELL_HEIGHT = HEIGHT // ROW_COUNT
@@ -16,13 +14,12 @@ CELL_HEIGHT = HEIGHT // ROW_COUNT
 screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
 pygame.display.set_caption("Level Builder")
 
-
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 
-
 bg = pygame.image.load("Images/BG.png")
 bg = pygame.transform.scale(bg, (WIDTH, HEIGHT))
+
 
 class Sprite:
     def __init__(self, x, y, image):
@@ -31,6 +28,7 @@ class Sprite:
 
     def draw(self, surface):
         surface.blit(self.image, self.rect.topleft)
+
 
 class Level:
     def __init__(self):
@@ -58,6 +56,7 @@ class Level:
             for row in grid:
                 f.write(' '.join(row) + '\n')
 
+
 def load_images_from_folder(folder):
     images = {}
     for filename in os.listdir(folder):
@@ -66,14 +65,60 @@ def load_images_from_folder(folder):
             images[filename] = img
     return images
 
+
 clock = pygame.time.Clock()
 level = Level()
-images = load_images_from_folder("Tile") 
+images = load_images_from_folder("Tile")
 current_image = None
-
 RUN = True
-menu_visible = False  
-scroll_y = 0  
+menu_visible = False
+
+scroll_y = 0
+
+def get_filename():
+    root = pygame.display.set_mode((400, 200))
+    pygame.display.set_caption("Enter filename")
+    font = pygame.font.Font(None, 36)
+    input_box = pygame.Rect(50, 80, 300, 50)
+    color_inactive = pygame.Color('lightskyblue3')
+    color_active = pygame.Color('dodgerblue2')
+    color = color_inactive
+    active = False
+    text = ''
+    done = False
+
+    while not done:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if input_box.collidepoint(event.pos):
+                    active = not active
+                else:
+                    active = False
+                color = color_active if active else color_inactive
+            if event.type == pygame.KEYDOWN:
+                if active:
+                    if event.key == pygame.K_RETURN:
+                        done = True
+                    elif event.key == pygame.K_BACKSPACE:
+                        text = text[:-1]
+                    else:
+                        text += event.unicode
+
+        root.fill((30, 30, 30))
+        txt_surface = font.render(text, True, color)
+        width = max(300, txt_surface.get_width() + 10)
+        input_box.w = width
+        root.blit(txt_surface, (input_box.x + 5, input_box.y + 5))
+        pygame.draw.rect(root, color, input_box, 2)
+        pygame.display.flip()
+        clock.tick(30)
+
+    pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
+    pygame.display.set_caption("Level Builder")
+    return text
 
 while RUN:
     for event in pygame.event.get():
@@ -82,68 +127,62 @@ while RUN:
             sys.exit()
 
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_f:  
+            if event.key == pygame.K_f:
                 if screen.get_flags() & pygame.FULLSCREEN:
                     screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
                 else:
                     screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 
-            
             if event.key == pygame.K_m:
                 menu_visible = not menu_visible
-            
+
+            if event.key == pygame.K_s:
+                filename = get_filename()
+                level.save_to_file(filename + ".txt")
+
             if event.key == pygame.K_ESCAPE:
                 RUN = False
 
         if event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1 and current_image is not None:  
+            if event.button == 1 and current_image is not None:
                 pos = event.pos
                 grid_x = int(pos[0] // CELL_WIDTH)
                 grid_y = int(pos[1] // CELL_HEIGHT)
                 level.add_sprite(grid_x, grid_y, current_image)
-            elif event.button == 3:  
+            elif event.button == 3:
                 level.remove_sprite(event.pos)
 
-            
             if menu_visible:
                 for i, (name, img) in enumerate(images.items()):
                     item_rect = pygame.Rect(10, 60 + i * (img.get_height() + 10) + scroll_y, img.get_width(), img.get_height())
                     if item_rect.collidepoint(event.pos):
                         current_image = img
 
-        
-        if event.type == pygame.MOUSEBUTTONDOWN and menu_visible and event.button == 4:  
+        if event.type == pygame.MOUSEBUTTONDOWN and menu_visible and event.button == 4:
             scroll_y += 10
-        elif event.type == pygame.MOUSEBUTTONDOWN and menu_visible and event.button == 5:  
+        elif event.type == pygame.MOUSEBUTTONDOWN and menu_visible and event.button == 5:
             scroll_y -= 10
 
-    
     bg_resized = pygame.transform.scale(bg, (WIDTH, HEIGHT))
     screen.blit(bg_resized, (0, 0))
 
-    
     level.draw(screen)
 
-    
     for x in range(0, WIDTH, CELL_WIDTH):
         pygame.draw.line(screen, (200, 200, 200), (x, 0), (x, HEIGHT))
     for y in range(0, HEIGHT, CELL_HEIGHT):
         pygame.draw.line(screen, (200, 200, 200), (0, y), (WIDTH, y))
 
-    
     menu_button_rect = pygame.Rect(WIDTH - 150, 10, 140, 40)
     pygame.draw.rect(screen, BLACK if not menu_visible else WHITE, menu_button_rect)
     font = pygame.font.Font(None, 36)
     button_text = font.render("Menu (press M)", True, WHITE if not menu_visible else BLACK)
     screen.blit(button_text, (WIDTH - 140, 15))
 
-    
     if menu_visible:
         for i, (name, img) in enumerate(images.items()):
             item_rect = pygame.Rect(10, 60 + i * (img.get_height() + 10) + scroll_y, img.get_width(), img.get_height())
             screen.blit(img, item_rect.topleft)
-
-            
             text_surface = font.render(name, True, BLACK)
             text_rect = text_surface.get_rect(topleft=(item_rect.right + 10, item_rect.top))
             screen.blit(text_surface, text_rect)
